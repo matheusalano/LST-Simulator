511b9397cd1ba3bec334875fef40b9fa233ac95d
Matheus Alano <matheus.alano.dias@gmail.com>
2019-09-01 19:14:23 -0300

    Talvez pronto?

--- lst.c
+++ lst.c
@@ -21,6 +21,15 @@ typedef struct {
   states s;
 } TASK;
 
+int indexOfTaskByID(TASK t[], int n, char id) {
+  for (int i=0; i < n; i++) {
+    if (t[i].id == id || t[i].id_d == id) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 int main() {
   TASK tasks[MAX_N];
   int n, t, i;
@@ -46,7 +55,28 @@ int main() {
     num_trocas_cont = 0;
 
     for (i = 0; i < t; i++) {
+      for (int j=0; j<n; j++) {
+        if ((tasks[j].c_left > 0) && (i%tasks[j].d == 0) && i > 0) {
+          tasks[j].s = DELAYED;              
+        }
+        else if (tasks[j].c_left == 0) {
+          tasks[j].s = DONE;
+        }
+        if ((i%tasks[j].p == 0) && (tasks[j].s != DELAYED) && i > 0) {
+          tasks[j].c_left = tasks[j].c;
+          tasks[j].slack = tasks[j].d - tasks[j].c;
+          tasks[j].s = READY;
+        }
+      }
+
         int lst = 0;
+
+        for (int j=0; j<n; j++) {
+          if (tasks[j].s != DONE) {
+            lst = j;
+            break;
+          }
+        }
 
         for (int j=0; j<n; j++) { //Varre as tarefas e salva aquela disponivel com menor slack
           if ((tasks[j].slack < tasks[lst].slack) && (tasks[j].s != DONE)) {
@@ -54,59 +84,37 @@ int main() {
           }
         }
 
-        // printf("LST:%d \n", lst);
-
         for (int j=0; j<n; j++) { //Quando for a menor tarefa, faz algo. Senao, faz algo.
-        printf("\nAQUI 60");
           if (j == lst) {
-            printf("\nAQUI 62");
             if (tasks[j].s == DONE) {
-              printf("\nAQUI 64");
               grade[i] = '.';
               if (grade[i - 1] != '.') {
-                printf("\nAQUI 65");
                 num_preemp++;
+                num_trocas_cont++;
               }
             } else {
-              printf("\nAQUI 71");
-              tasks[j].c_left--;
+              tasks[j].c_left = tasks[j].c_left - 1;
+              if ((grade[i - 1] != tasks[j].id) && (tasks[indexOfTaskByID(tasks, n, grade[i - 1])].c_left > 0)) { num_preemp++; }
               if (tasks[j].s == DELAYED) {
-                printf("\nAQUI 74");
                 grade[i] = tasks[j].id_d;
               } else {
-                printf("\nAQUI 77\n");
+                if (grade[i - 1] == tasks[j].id_d) { num_trocas_cont++; }
+                if (grade[i - 1] != tasks[j].id && grade[i - 1] != tasks[j].id_d) { num_trocas_cont++; }
                 grade[i] = tasks[j].id;
               }
             }
           } else {
-            printf("\nAQUI 82");
             if (tasks[j].s != DONE) {
-              printf("\nAQUI 84");
               tasks[j].slack--;
             }
           }
-        }
-
-        for (int j=0; j<n; j++) {
-            if ((tasks[j].c_left > 0) && (i%tasks[j].d == 0)) {
-              tasks[j].s = DELAYED;              
-            }
-            else if (tasks[j].c_left == 0) {
-              tasks[j].s = DONE;
-            }
-            if ((i%tasks[j].p == 0) && (tasks[j].s != DELAYED)) {
-              tasks[j].c_left = tasks[j].c;
-              tasks[j].slack = tasks[j].d - tasks[j].c;
-              tasks[j].s = READY;
-            }
         }
     }
 
     grade[t] = '\0';
     
     // RESULTADOS
-    printf("%s\n%d %d\n\n", grade, num_preemp, num_trocas_cont);
-    return 0;
+    printf("%s\n%d %d\n\n", grade, num_trocas_cont, num_preemp);
   }
   return 0;
 }
